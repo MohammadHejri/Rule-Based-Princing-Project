de9985ead5332589a5e19e616441cbff4ed45abc
Mohammad <mohammadhejri.ce@gmail.com>
2022-08-18 17:32:20 +0430

    Minor changes

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ Base = declarative_base()
 
 
 def createNewSession():
-    DATABASE_URL = "sqlite:///./database3.db"
+    DATABASE_URL = "sqlite:///./database.db"
     engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
     Base.metadata.create_all(engine)
     session = sessionmaker(bind=engine)()
